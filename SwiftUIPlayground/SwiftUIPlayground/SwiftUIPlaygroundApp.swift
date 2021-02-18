fab6222085c4292e11a7cc5ccc3a25ee2cb4c655
Muukii <muukii.app@gmail.com>
2021-02-19 02:52:15 +0900

    :evergreen_tree: Update

---
+++ SwiftUIPlayground/SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Muukii on 2021/02/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
