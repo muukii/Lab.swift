961133c67c40aea86f33a740aadeef0c7d862d6a
Muukii <muukii.app@gmail.com>
2021-10-24 16:40:28 +0900

    Wip

--- IsolatedPackages/SyntaxBuilder/SyntaxBuilder/Run.swift
+++ IsolatedPackages/SyntaxBuilder/SyntaxBuilder/Run.swift
@@ -7,16 +7,58 @@
 
 import Foundation
 import SwiftSyntax
+import SwiftSyntaxBuilder
 
 enum Playground {
 
   static func run() {
 
-    class Parser: SyntaxRewriter {
+    final class EasyPeasyLayout: SyntaxRewriter {
+
+      override func visit(_ token: TokenSyntax) -> Syntax {
+        print(token)
+        return super.visit(token)
+      }
 
     }
 
-    let source = """
+    final class Parser: SyntaxRewriter {
+
+      override func visit(_ node: FunctionCallExprSyntax) -> ExprSyntax {
+//        print(node.calledExpression)
+        if let a = node.calledExpression.as(MemberAccessExprSyntax.self) {
+
+          let expression = a.tokens
+            .map { $0.text }
+            .joined(separator: "")
+
+          if expression.hasSuffix("easy.layout") {
+            let modified = EasyPeasyLayout().visit(a)
+            return .init(modified)
+//            return super.visit(.init(modified))
+          }
+
+        }
+
+        return super.visit(node)
+      }
+
+      override func visit(_ node: FunctionDeclSyntax) -> DeclSyntax {
+//        let a = node.withIdentifier(SyntaxFactory.makeIdentifier("_\(node.identifier)"))
+        return super.visit(node)
+      }
+    }
+
+    let source =
+"""
+func run() {
+ topToBottomGradientView.easy.layout(
+   Top().to(scrollView, .top),
+   Left().to(scrollView, .left),
+   Right(indicatorInset).to(scrollView, .right),
+   Height(4.bk)
+  )
+}
 """
 
     let parsed = try! SyntaxParser.parse(source: source)
